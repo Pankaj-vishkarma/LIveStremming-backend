25ee468495d0f6dbc3528217f9da7c39b429928a
Arun Kumar <arun.etech2011@gmail.com>
2026-04-07 12:11:42 +0530

    increase upload avatar size 5 to 10 mb

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-// src/app.js
+
 
 const express = require("express");
 const cors = require("cors");
@@ -50,7 +50,7 @@ app.use(
 
 app.use(
     cors({
-        origin: [process.env.FRONTEND_URL], // production me restrict karna
+        origin: [process.env.FRONTEND_URL],
         credentials: true,
     })
 );
